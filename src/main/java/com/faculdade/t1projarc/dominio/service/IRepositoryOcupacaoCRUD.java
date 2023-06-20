9afc9c897d8ad9a170a1f198e3721987b65d2c5a
Eduardo Ballico <ballico591@gmail.com>
2023-06-20 11:29:15 -0300

    deleteando

--- src/main/java/com/faculdade/t1projarc/dominio/service/IRepositoryOcupacaoCRUD.java
+++
@@ -1,8 +0,0 @@
-package com.faculdade.t1projarc.dominio.service;
-
-import com.faculdade.t1projarc.interfacesAdaptadoras.repository.IRepositoryOcupacao;
-import org.springframework.stereotype.Component;
-
-@Component
-public interface IRepositoryOcupacaoCRUD extends IRepositoryOcupacao {
-}
--- src/main/java/com/faculdade/t1projarc/dominio/service/IRepositoryRotasCRUD.java
+++
@@ -1,8 +0,0 @@
-package com.faculdade.t1projarc.dominio.service;
-
-import com.faculdade.t1projarc.interfacesAdaptadoras.repository.IRepositoryRotas;
-import org.springframework.stereotype.Component;
-
-@Component
-public interface IRepositoryRotasCRUD extends IRepositoryRotas {
-}
